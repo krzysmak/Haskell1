edb9c28fb4d156cc4d33343aa0269c4effaf1847
Krzysztof Makuracki <krzysmak@onet.pl>
2023-03-23 00:26:17 +0100

    instance Polynomial Sparse Poly

--- SparsePoly-template.hs
+++ SparsePoly-template.hs
@@ -1,6 +1,9 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use second" #-}
 module SparsePoly(fromDP, toDP, qrP) where
 import PolyClass
 import Representation
+import Data.List
 
 -- | fromDP example
 -- >>> fromDP sampleDP
@@ -16,9 +19,31 @@ first = undefined
 second :: (b -> b') -> (a, b) -> (a, b')
 second = undefined
 
+simplify :: (Eq a, Num a) => [(Int, a)] -> [(Int, a)]
+simplify [] = []
+simplify ((x, y) : xs)
+    | x == 0 = simplify xs
+    | otherwise = (x, y) : simplify xs
+
+sortAndSimplify :: (Eq a, Num a) => [(Int, a)] -> [(Int, a)]
+sortAndSimplify xs = simplify $ sortBy (\(x, _) (y, _) -> compare x y) xs
+
 instance Functor SparsePoly where
+    fmap f (S s) = S $ fmap (\(p, c) -> (p, f c)) s
 
 instance Polynomial SparsePoly where
+    zeroP = S []
+    constP x = S [(0, x)]
+    varP = S [(1, 1)]
+    evalP (S s) x = foldr (\(p, c) acc -> acc + c * (x^p)) 0 s
+    shiftP n (S s) = S $ fmap (\(p, c) -> (p + n, c)) (sortAndSimplify s)
+    degree (S s)
+        | null sorted = -1
+        | otherwise = x
+        where 
+            sorted = sortAndSimplify s
+            (x, _) = last sorted
+    nullP (S s) = null $ sortAndSimplify s
 
 instance (Eq a, Num a) => Num (SparsePoly a) where
 
