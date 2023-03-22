7bb78e7ad84061065fe2e8f300cd74fbdd20dde1
Krzysztof Makuracki <krzysmak@onet.pl>
2023-03-22 16:35:46 +0100

    Instance Num DensePoly

--- DensePoly-template.hs
+++ DensePoly-template.hs
@@ -5,13 +5,13 @@ import Representation
 instance Functor DensePoly where
     fmap f (P p) = P (map f p)
 
-removeLeadingZeros :: Num a => [a] -> [a]
+removeLeadingZeros :: (Eq a, Num a) => [a] -> [a]
 removeLeadingZeros [] = []
 removeLeadingZeros (x : xs)
     | x == 0 = removeLeadingZeros xs
-    | otherwise = (x : xs)
+    | otherwise = x : xs
 
-removeTrailingZeros :: Num a => [a] -> [a]
+removeTrailingZeros :: (Eq a, Num a) => [a] -> [a]
 removeTrailingZeros p = removeLeadingZeros $ reverse p
 
 instance Polynomial DensePoly where
@@ -20,11 +20,38 @@ instance Polynomial DensePoly where
     varP = P [0, 1]
     evalP (P p) x = foldr (\a acc -> a + acc * x) 0 p
     shiftP n (P p) = P (removeTrailingZeros (replicate n 0 ++ p))
-    degree (P p) = (length $ removeTrailingZeros p) - 1
+    degree (P p) = length (removeTrailingZeros p) - 1
     nullP (P p) = all (== 0) p
 
-instance (Eq a, Num a) => Num (DensePoly a) where
+addArrays :: Num a => [a] -> [a] -> [a]
+addArrays [] [] = []
+addArrays x [] = x
+addArrays [] x = x
+addArrays (x : xs) (y : ys) = (x + y) : addArrays xs ys
 
+subArrays :: Num a => [a] -> [a] -> [a]
+subArrays [] [] = []
+subArrays x [] = x
+subArrays [] (x : xs) =  (-1 * x) : subArrays [] xs
+subArrays (x : xs) (y : ys) = (x - y) : subArrays xs ys
+
+mulArraysWithIndex :: Num a => Int -> [a] -> [a] -> [a]
+mulArraysWithIndex _ _ [] = []
+mulArraysWithIndex _ [] _ = []
+mulArraysWithIndex i (x : xs) ys = addArrays firstArray (mulArraysWithIndex (i + 1) xs ys)
+    where firstArray = replicate i 0 ++ map (* x) ys
+
+mulArrays :: Num a => [a] -> [a] -> [a]
+mulArrays = mulArraysWithIndex 0
+
+instance (Eq a, Num a) => Num (DensePoly a) where
+    P p1 + P p2 = P (removeTrailingZeros (addArrays p1 p2))
+    P p1 - P p2 = P (removeTrailingZeros (subArrays p1 p2))
+    abs = undefined
+    signum = undefined
+    fromInteger e = constP $ fromIntegral e
+    P p1 * P p2 = P (removeTrailingZeros (mulArrays p1 p2))
+    
 
 -- |
 -- >>> x^3 - 1 :: DensePoly Integer 
--- mytester.hs
+++ mytester.hs
@@ -19,16 +19,46 @@ removeLeadingZeros :: (Eq a, Num a) => [a] -> [a]
 removeLeadingZeros [] = []
 removeLeadingZeros (x : xs)
     | x == 0 = removeLeadingZeros xs
-    | otherwise = (x : xs)
+    | otherwise = x : xs
 
 removeTrailingZeros :: (Eq a, Num a) => [a] -> [a]
-removeTrailingZeros p = removeLeadingZeros $ reverse p
+removeTrailingZeros p = reverse $ removeLeadingZeros $ reverse p
 
 instance Polynomial DensePoly where
     zeroP = P []
     constP x = P [x]
     varP = P [0, 1]
     evalP (P p) x = foldr (\a acc -> a + acc * x) 0 p
-    shiftP n (P p) = P (replicate n 0 ++ p)
-    degree (P p) = (length $ removeTrailingZeros p) - 1
+    shiftP n (P p) = P (removeTrailingZeros (replicate n 0 ++ p))
+    degree (P p) = length (removeTrailingZeros p) - 1
     nullP (P p) = all (== 0) p
+
+addArrays :: Num a => [a] -> [a] -> [a]
+addArrays [] [] = []
+addArrays x [] = x
+addArrays [] x = x
+addArrays (x : xs) (y : ys) = (x + y) : addArrays xs ys
+
+subArrays :: Num a => [a] -> [a] -> [a]
+subArrays [] [] = []
+subArrays x [] = x
+subArrays [] (x : xs) =  (-1 * x) : subArrays [] xs
+subArrays (x : xs) (y : ys) = (x - y) : subArrays xs ys
+
+mulArraysWithIndex :: Num a => Int -> [a] -> [a] -> [a]
+mulArraysWithIndex _ _ [] = []
+mulArraysWithIndex _ [] _ = []
+mulArraysWithIndex i (x : xs) ys = addArrays firstArray (mulArraysWithIndex (i + 1) xs ys)
+    where firstArray = replicate i 0 ++ map (* x) ys
+
+mulArrays :: Num a => [a] -> [a] -> [a]
+mulArrays = mulArraysWithIndex 0
+
+instance (Eq a, Num a) => Num (DensePoly a) where
+    P p1 + P p2 = P (removeTrailingZeros (addArrays p1 p2))
+    P p1 - P p2 = P (removeTrailingZeros (subArrays p1 p2))
+    abs = undefined
+    signum = undefined
+    fromInteger e = constP $ fromIntegral e
+    P p1 * P p2 = P (removeTrailingZeros (mulArrays p1 p2))
+    
